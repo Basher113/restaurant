3ddc450a09e690777384d6f92b8ff574fcd52429
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-06-02 08:26:13 +0800

    Load css in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
+
 module.exports = {
     entry: "./src/script.js",
+    mode: "development",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "main.js"
+        filename: "main.js",
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({template: "./src/index.html"})
-    ] 
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
