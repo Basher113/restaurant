a725eac4355a35e869030ac8e83b79f5c61dc460
Basher113 <147272612+Basher113@users.noreply.github.com>
2025-06-03 08:16:14 +0800

    Add styling on tabs when active
    - Implement function for setting active tabs to style the tab element

--- src/script.js
+++ src/script.js
@@ -8,27 +8,40 @@ function ContentController() {
     const tabsList = document.querySelectorAll(".tab");
     const contentDiv = document.querySelector("#content")
     const homePage = HomePage();
-    
+
 
     const renderPage = (content=homePage) => {
         contentDiv.textContent = "";
         contentDiv.appendChild(content);
+        
     }
 
     tabsList.forEach(tab => {
+        
         tab.addEventListener("click", () => {
+            setActiveTab(tab);
             if (tab.textContent === "Menu") {
                 const menuPage = MenuPage();
-                renderPage(menuPage)
-            } else if (tab.textContent === "Home") {
-                renderPage()
-            } else {
+                renderPage(menuPage);
+            } else if (tab.textContent === "Contact") {
                 const contactPage = ContactPage();
                 renderPage(contactPage)
+            } else {
+                renderPage()
             }
         })
     })
-    renderPage()
+
+    const setActiveTab = (tab) => {
+        tabsList.forEach(tabEl => {
+            tab === tabEl ? tabEl.classList.add("active") : tabEl.classList.remove("active");
+        })
+    }
+
+    // Initialize the active tab for home
+    setActiveTab(tabsList[0]);
+    
+    renderPage();
 }
 
 ContentController()
